4aaad1881f60ca7aa24ffcca47e8cf07e09f66e8
Leeza Luncheon <leezaluncheon@gmail.com>
2018-03-14 01:21:18 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,30 @@
-# your code goes here
+require "pry"
+
+def begins_with_r(array)
+  array.all? { |element|element.start_with?("r")}
+
+#  binding.pry
+  
+  
+end
+
+def contain_a(array)
+  array.select{|element|element.include?("a")}
+end
+
+def first_wa(array)
+  array.find{|element|element.to_s.start_with?("wa")}
+  
+end
+
+def remove_non_strings(array)
+ array.select{|element|element.is_a?(String)}
+end
+
+def count_elements(array, query)
+  count=0
+  array.each do |element| 
+    count+=1 if element = query
+  end
+  count
+end
